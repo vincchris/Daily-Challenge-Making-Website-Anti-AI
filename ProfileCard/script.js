e13447579daddba2d4dc795fa91d6ecfd98f1358
Vincent Chriss <vchristianthen@gmail.com>
2026-09-09 21:10:46 +0700

    Update Progress 01

--- ProfileCard/script.js
+++ ProfileCard/script.js
@@ -1,12 +1,44 @@
 const followButton = document.querySelector("#followButton")
 const followers = document.querySelector("#followers")
+const messageButton = document.querySelector("#messageButton")
+const formInput = document.getElementById("formInput")
+const sendBtn = document.getElementById("sendBtn")
+const msgInput = document.getElementById("msgInput")
+const messageList = document.getElementById("messageList")
 
 let isFollowing = false
 
 followButton.addEventListener('click', function () {
-  isFollowing = true
-  if (isFollowing) {
-    followButton.innerHTML = "Following"
-    followers.innerText += 1
+  let sumFollowers = parseInt(followers.textContent)
+  if (!isFollowing) {
+    isFollowing = true
+    followButton.textContent = "Following"
+    sumFollowers += 1
+    followers.textContent = sumFollowers
+  } else {
+    isFollowing = false
+    sumFollowers -= 1
+    followButton.textContent = "Follow"
+  }
+
+  followers.textContent = sumFollowers
+})
+
+messageButton.addEventListener("click", function() {
+  if (formInput.style.display === "none") {
+    formInput.style.display = "block"
+  } else {
+    formInput.style.display = "none"
+  }
+})
+
+sendBtn.addEventListener("click", function() {
+  if (msgInput.value.trim() !== "") {
+    const newMsg = document.createElement("p")
+    newMsg.textContent = msgInput.value
+
+    messageList.appendChild(newMsg)
+
+    msgInput.value = ""
   }
 })
